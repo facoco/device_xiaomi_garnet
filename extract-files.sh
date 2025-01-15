df2de89e5bbada1520f7b0bce5e3fd5465c9d925
Michael Bestas <mkbestas@lineageos.org>
2025-01-15 12:47:35 -0300

    garnet: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -83,7 +83,7 @@ function blob_fixup() {
             ;;
         vendor/etc/media_codecs_parrot_v0.xml)
             [ "$2" = "" ] && return 0
-            sed -i -E '/media_codecs_(google_audio|google_telephony|vendor_audio)/d' "${2}"
+            sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;      
         *)
             return 1
