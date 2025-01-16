c3d8495457a800ef3cff7b70c582bb76e62b76e1
Michael Bestas <mkbestas@lineageos.org>
2025-01-15 23:24:20 -0300

    garnet: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- aosp_garnet.mk
+++ aosp_garnet.mk
@@ -8,6 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+TARGET_SUPPORTS_OMX_SERVICE := false
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common aosp stuff.
