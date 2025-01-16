3ca8c94455a388753133f37a77350a963346056f
kleidione Freitas <kleidione@gmail.com>
2025-01-15 22:47:43 -0300

    garnet: Start PixelOS
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -394,9 +394,6 @@ PRODUCT_PACKAGES += \
     FrameworkOverlayGarnet \
     FrameworkOverlayGarnetGLEsim \
     FrameworkOverlayGarnetJPEsim \
-    LineageSDKOverlayGarnet \
-    LineageSettingsOverlayGarnet \
-    LineageSystemUIOverlayGarnet \
     SettingsOverlayGarnet \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
