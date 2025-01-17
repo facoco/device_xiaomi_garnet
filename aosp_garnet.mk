1096ff2b9a5b03b378d8f4754bf1858cddf42cc1
kleidione Freitas <kleidione@gmail.com>
2025-01-17 19:58:23 -0300

    garnet: Update from V816.0.17.0.UNRMIXM
    Signed-off-by: kleidione <kleidione@gmail.com>

--- aosp_garnet.mk
+++ aosp_garnet.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_NAME := garnet_global
 PRODUCT_SYSTEM_DEVICE := garnet
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="garnet_global-user 14 UKQ1.231003.002 V816.0.9.0.UNRMIXM release-keys" \
-    BuildFingerprint=Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.9.0.UNRMIXM:user/release-keys \
+    BuildDesc="garnet_global-user 14 UKQ1.231003.002 V816.0.17.0.UNRMIXM release-keys" \
+    BuildFingerprint=Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.17.0.UNRMIXM:user/release-keys \
     DeviceName=garnet \
     DeviceProduct=garnet_global
 
